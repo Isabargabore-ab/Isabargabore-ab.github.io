568e70dc88564e143ee8e6545799faac7b6b67df
Tobil01 <tobil.isabar@hotmail.com>
2025-02-10 15:25:44 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 document.getElementById("explore-btn").addEventListener("click", function() {
     var aboutSection = document.getElementById("about");
-    aboutSection.classList.toggle("show");
+
+    if (aboutSection.style.display === "none" || aboutSection.style.display === "") {
+        aboutSection.style.display = "block";
+        setTimeout(() => { aboutSection.style.opacity = "1"; }, 10);
+    } else {
+        aboutSection.style.opacity = "0";
+        setTimeout(() => { aboutSection.style.display = "none"; }, 500);
+    }
 });
