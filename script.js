4756021fd1cc9049e075e7d7ae52481b26770ad3
Tobil01 <tobil.isabar@hotmail.com>
2025-02-10 15:23:00 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("explore-btn").addEventListener("click", function() {
+    var aboutSection = document.getElementById("about");
+    aboutSection.classList.toggle("show");
+});
